cfd89dd0dbfb29ed70027abd5f0e95008994bade
felri <felipercmelo@gmail.com>
2023-01-27 15:55:25 -0300

    remove cors

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from routes import checks, joyplot, treemap
 
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
