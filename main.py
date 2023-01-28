f6861a3068854603cdda42d87a097bf3b508458d
felri <felipercmelo@gmail.com>
2023-01-28 13:02:16 -0300

    fix redis read only

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from routes import checks, joyplot, treemap
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://dadoslegiveis.lol",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
