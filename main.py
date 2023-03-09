93d33ae3d3ec4ebc66388372b71a3022da4e83b7
felri <felipercmelo@gmail.com>
2023-03-09 08:25:51 -0300

    fix script not running daily

--- main.py
+++ main.py
@@ -23,30 +23,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 @repeat_every(seconds=86400)  # 24 hours
 def update_latest_entries_in_csv() -> None:
-    if os.environ.get("DONT_RUN_SCRIPTS") != "dev":
-        downloader.download_expenses_current_year()
-        downloader.format_csv_data_to_db()
+    downloader.download_expenses_current_year()
+    downloader.format_csv_data_to_db()
 
-        current_date = pd.to_datetime("today").date()
-        updater.delete_entries_after_date(current_date)
-        latest_date = updater.get_latest_date()
-        df = updater.get_df_from_csv("./datasets/expenses/")
-        # Convert string dates to datetime.date objects
-        df["datEmissao"] = pd.to_datetime(
-            df["datEmissao"], format="%Y-%m-%dT%H:%M:%S"
-        ).dt.date
-        # remove all the entries with datEmissao after the current date
-        df = df[df["datEmissao"] <= current_date]
-        print(f"{len(df)} entries in CSV.")
-        print(f"current date: {current_date}")
-        print(f"latest date: {latest_date}")
-        # Filter the rows with datEmissao after the latest date
-        df = df[df["datEmissao"] > latest_date]
-        updater.save_to_db(df)
-        print(f"Updated {len(df)} entries.")
+    current_date = pd.to_datetime("today").date()
+    updater.delete_entries_after_date(current_date)
+    latest_date = updater.get_latest_date()
+    df = updater.get_df_from_csv("./datasets/expenses/")
+    # Convert string dates to datetime.date objects
+    df["datEmissao"] = pd.to_datetime(
+        df["datEmissao"], format="%Y-%m-%dT%H:%M:%S"
+    ).dt.date
+    # remove all the entries with datEmissao after the current date
+    df = df[df["datEmissao"] <= current_date]
+    print(f"{len(df)} entries in CSV.")
+    print(f"current date: {current_date}")
+    print(f"latest date: {latest_date}")
+    # Filter the rows with datEmissao after the latest date
+    df = df[df["datEmissao"] > latest_date]
+    updater.save_to_db(df)
+    print(f"Updated {len(df)} entries.")
 
 
 @app.get("/")
